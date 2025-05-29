a8a7372e5d04753043f18e2f5cb6f90f0f58cb29
ItsProbablyAndy <andrew.pm85@gmail.com>
2025-05-29 02:18:21 +0100

    about update

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
           form.reset();
         })
         .catch(error => {
-          alert("Thank you for your message!");
+          alert("There was an error sending your message."); // I genuinely don't know why but emails still send so error is success
           console.error(error);
         });
     });
